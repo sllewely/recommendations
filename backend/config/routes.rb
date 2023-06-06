b6432906dc1e01df0e2fc57004785273b439377e
Sarah Llewelyn <se.llewelyn@gmail.com>
2023-06-06 09:21:27 -0400

    devise controllers

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tests
+      devise_for :users
     end
   end
 end

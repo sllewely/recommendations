8c65e3787c4632d9110a5bdc1e6d66605cb8d67a
Sarah Llewelyn <se.llewelyn@gmail.com>
2023-06-07 18:34:17 -0400

    list and create media

--- backend/app/controllers/api/v1/media_controller.rb
+++ backend/app/controllers/api/v1/media_controller.rb
@@ -6,12 +6,13 @@ class Api::V1::MediaController < Api::V1::ApiController
   end
 
   def create
-    @medium = Medium.new(medium_params.merge!(user_id: current_user.id))
+    @medium = Medium.find_or_create_by(name: medium_params[:name])
+    @user_medium = UserMedium.new(notes: medium_params[:notes], user_id: current_user.id, medium_id: @medium.id)
     # Create UserMedia
     # Find or Create media
     # Use media type
-    
-    if @medium.save
+
+    if @user_medium.save
       render json: @medium
     else
       render error: { error: 'Unable to create Medium.'}, status: 400

5d20e5325c940227f150a998c880d7bd7cbc8903
Fiona Conn <fionapwconn@gmail.com>
2023-06-07 13:01:14 -0400

    Make the friends method (#15)

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  has_many :friends_requested, class_name: 'Friendship', foreign_key: 'from_user_id', inverse_of: :requestor
-  has_many :friends_possible, -> { where accepted_at: nil }, class_name: 'Friendship', through: :friends_requested
-  has_many :friends_accepted, -> { where.not accepted_at: nil }, class_name: 'Friendship', through: :friends_requested
+  has_many :friendships_requested, class_name: 'Friendship', foreign_key: 'from_user_id', inverse_of: :requestor
+  has_many :friends_requested, class_name: 'User', through: :friendships_requested, source: :requested
 
-  has_many :friend_requests, class_name: 'Friendship', foreign_key: 'to_user_id', inverse_of: :requested
-  has_many :pending_friends, -> { where accepted_at: nil }, class_name: 'Friendship', through: :friend_requests
-  has_many :accepted_friends, -> { where.not accepted_at: nil }, class_name: 'Friendship', through: :friend_requests
+  has_many :friendship_requests, class_name: 'Friendship', foreign_key: 'to_user_id', inverse_of: :requested
+  has_many :friend_requests, class_name: 'User', through: :friendship_requests, source: :requestor
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -17,5 +15,12 @@ class User < ApplicationRecord
   def self.authenticate(email, password)
     user = User.find_for_authentication(email: email)
     user&.valid_password?(password) ? user : nil
+  end
+
+  def friends
+    friends_one = friends_requested.where('friendships.accepted_at IS NOT NULL').select(:id).to_sql
+    friends_two = friend_requests.where('friendships.accepted_at IS NOT NULL').select(:id).to_sql
+    subquery = [friends_one, friends_two].join(" UNION ")
+    User.where("id IN (#{subquery})")
   end
 end

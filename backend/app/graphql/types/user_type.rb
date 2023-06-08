317a96b6d6d16f946c51a351b216125386a6b069
Sarah Llewelyn <se.llewelyn@gmail.com>
2023-06-08 12:31:14 -0400

    remove secret user fields

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -3,11 +3,11 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String, null: false
-    field :encrypted_password, String, null: false
-    field :reset_password_token, String
-    field :reset_password_sent_at, GraphQL::Types::ISO8601DateTime
-    field :remember_created_at, GraphQL::Types::ISO8601DateTime
+    # field :email, String, null: false
+    # field :encrypted_password, String, null: false
+    # field :reset_password_token, String
+    # field :reset_password_sent_at, GraphQL::Types::ISO8601DateTime
+    # field :remember_created_at, GraphQL::Types::ISO8601DateTime
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :first_name, String

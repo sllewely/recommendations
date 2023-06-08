2befaa7f39f8f3b76fcc2086de820cd02b8659f4
Jasmine <jasminee@gmail.com>
2023-06-08 12:35:26 -0400

    added form validation to login page

--- web/src/app/account/login/page.tsx
+++ web/src/app/account/login/page.tsx
@@ -1,19 +1,31 @@
-import Link from "next/link"
-import styles from "../styles.module.css"
+"use client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import styles from "../styles.module.css";
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className='w-96 mt-4 mx-auto border-solid-md border-2 rounded border-sky-500 p-4 bg-white'>
       <div className='text-2xl'>Login</div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className='my-3'>
           <label className='text-l'> Email</label>
           <br />
           <input
             className='border-solid border-2 rounded border-gray w-full p-2'
-            name='email'
-            type='text'
-          ></input>
+            // type='text'
+            // name='email'
+            {...register("email", { required: true })}
+          />
+          {errors?.email?.type === "required" && <p>This field is required</p>}
         </div>
 
         <div className='my-3'>
@@ -21,13 +33,21 @@ export default function Login() {
           <br />
           <input
             className='border-solid border-2 rounded border-gray w-full p-2'
-            name='password'
             type='password'
-          ></input>
+            {...register("password", { required: true, minLength: 3 })}
+            // keeping pw length short but will change later
+          />
+          {errors?.password?.type === "required" && (
+            <p>This field is required</p>
+          )}
+          {errors?.password?.type === "minLength" && (
+            <p>Password must be at least 3 characters long</p>
+          )}
         </div>
-        <button className='border-solid border-2 rounded bg-blue-600 hover:bg-blue-700 w-full text-white p-2 my-4'>
-          Login
-        </button>
+        <input
+          type='submit'
+          className='border-solid border-2 rounded bg-blue-600 hover:bg-blue-700 w-full text-white p-2 my-4'
+        ></input>
       </form>
       <div className='text-center'>
         New User?

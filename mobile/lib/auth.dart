597dd1b167e2b0d3ee82c7418fca6f778780f5d0
Fiona Conn <fionapwconn@gmail.com>
2023-07-17 18:37:05 -0400

    Sign up sign in (#32)

--- mobile/lib/auth.dart
+++ mobile/lib/auth.dart
@@ -14,11 +14,15 @@ class AuthConfig {
   final String signInUrl;
   final String signUpUrl;
   final String tokenUrl;
+  final String clientId;
+  final String clientSecret;
 
   const AuthConfig({
     required this.signInUrl,
     required this.signUpUrl,
-    required this.tokenUrl
+    required this.tokenUrl,
+    required this.clientId,
+    required this.clientSecret,
   });
 }
 
@@ -26,29 +30,69 @@ abstract class BaseUser {
   String? firstName;
   String? lastName;
   String? email;
-
-  bool isSignedIn();
-
+  String? errors;
+  bool isSignedIn() => false;
+  bool hasErrors() => errors != null;
+  
+  BaseUser({this.firstName, this.lastName, this.email});
   Map<String, String?> toMap() {
+    Map<String, dynamic> map = {};
+    if (firstName != null) {
+      map['firstName'] = firstName;
+    }
+    if (lastName != null) {
+      map['lastName'] = lastName;
+    }
     return {
-      'firstName': firstName,
-      'lastName': lastName,
+      ...map,
+      'email': email,
+    };
+  }
+
+  Map<String, String?> toHttpMap() {
+    Map<String, dynamic> map = {};
+    if (firstName != null) {
+      map['first_name'] = firstName;
+    }
+    if (lastName != null) {
+      map['last_name'] = lastName;
+    }
+    return {
+      ...map,
       'email': email,
     };
   }
 }
 
 class AnonymousUser extends BaseUser {
+  String? password;
 
+  AnonymousUser ({this.password, email, firstName, lastName});
   @override
   bool isSignedIn() {
     return false;
+  }
+
+  @override
+  Map<String, String?> toMap() {
+    return {
+      ...super.toMap(),
+      'password': password,
+    };
+  }
+
+  @override
+  Map<String, String?> toHttpMap() {
+    return {
+      ...super.toHttpMap(),
+      'password': password,
+    };
   }
 }
 
 class User extends BaseUser {
   @override
-  User({required firstName, required lastName, required email});
+  User({firstName, lastName, required email});
 
   @override
   bool isSignedIn() {
@@ -57,23 +101,36 @@ class User extends BaseUser {
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
-        firstName: map['firstName'],
-        lastName: map['lastName'],
+        firstName: map.containsKey('firstName') ? map['firstName'] : null,
+        lastName: map.containsKey('lastName') ? map['lastName'] : null,
         email: map['email']
     );
   }
 }
 
-class UserStorage {
+class LocalStorage {
   String key;
 
   BaseStorage storage = createStorage();
 
-  UserStorage(this.key, {BaseStorage? storage}) {
+  LocalStorage(this.key, {BaseStorage? storage}) {
     if (storage != null) this.storage = storage;
   }
 
-  Future<BaseUser> getUser() async {
+  Future<void> insert(obj) async {
+    await storage.write(key, jsonEncode(obj.toMap()));
+  }
+
+  Future<void> remove() async {
+    await storage.write(key, '');
+  }
+}
+
+class UserStorage extends LocalStorage {
+  UserStorage(super.key);
+
+
+  Future<BaseUser> get() async {
     BaseUser user = AnonymousUser();
     final serializedStoredUser = await storage.read(key);
 
@@ -85,12 +142,90 @@ class UserStorage {
     }
     return user;
   }
+}
 
-  Future<void> insertUser(User user) async {
-    await storage.write(key, jsonEncode(user.toMap()));
+class Token {
+  String? accessToken;
+  String? tokenType;
+  String? refreshToken;
+  int? expirationMs;
+  String? errors;
+
+  bool hasErrors() {
+    return errors != null;
   }
 
-  Future<void> removeUser() async {
-    await storage.write(key, '');
+  Token({this.accessToken, this.tokenType, this.refreshToken, this.expirationMs});
+
+  DateTime? get expirationDate {
+    DateTime? expirationDt;
+    if (expirationMs != null) {
+      expirationDt = DateTime.fromMillisecondsSinceEpoch(expirationMs!);
+    }
+    return expirationDt;
   }
+
+  bool isRefreshNeeded({secondsToExpiration = 30}) {
+    bool needsRefresh = false;
+
+    if (expirationDate != null) {
+      DateTime now = DateTime.now();
+      needsRefresh = expirationDate!.difference(now).inSeconds < secondsToExpiration;
+    }
+
+    return needsRefresh;
+  }
+
+  bool isEmpty() {
+    return refreshToken == null;
+  }
+
+  bool isValid() {
+    return !isEmpty() && !isRefreshNeeded();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "accessToken": accessToken,
+      "tokenType": tokenType,
+      "refreshToken": refreshToken,
+      "expirationMs": expirationMs,
+    };
+  }
+
+  factory Token.fromMap(Map<String, dynamic> map) {
+    return Token(
+      accessToken: map['accessToken'],
+      tokenType: map['tokenType'],
+      refreshToken: map['refreshToken'],
+      expirationMs: map['expirationMs'],
+    );
+  }
+
+  factory Token.fromHttp(Map<String, dynamic> map) {
+    return Token.fromMap({
+      'accessToken': map['access_token'],
+      'tokenType': map['token_type'],
+      'refreshToken': map['refresh_token'],
+      'expirationMs': map['created_at']*1000 + map['expires_in']*1000,
+    });
+  }
+}
+
+class TokenStorage extends LocalStorage {
+  Token token = Token();
+  TokenStorage(super.key);
+
+  Future<Token> get() async {
+    final serializedStoredToken = await storage.read(key);
+
+    if (serializedStoredToken != null) {
+      final storedToken = jsonDecode(serializedStoredToken);
+      if (storedToken != null) {
+        token = Token.fromMap(storedToken);
+      }
+    }
+    return token;
+  }
+
 }
--- mobile/lib/auth_helper.dart
+++ mobile/lib/auth_helper.dart
@@ -1,37 +1,250 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:mobile/auth.dart';
+import 'package:http/http.dart' as http;
+
 
 class AuthHelper extends ChangeNotifier {
   final AuthConfig authConfig;
   late UserStorage userStorage;
+  late TokenStorage tokenStorage;
 
   BaseUser _user = AnonymousUser();
   BaseUser get currentUser => _user;
+  Token _token = Token();
+  Token get currentToken => _token;
 
-  AuthHelper(this.authConfig, {userStorage}) {
-    this.userStorage = userStorage ?? UserStorage(authConfig.tokenUrl);
+  AuthHelper(this.authConfig, {userStorage, tokenStorage}) {
+    this.userStorage = userStorage ?? UserStorage('user');
+    this.tokenStorage = tokenStorage ?? TokenStorage(authConfig.tokenUrl);
+
     fetchUser();
+    fetchToken();
   }
 
   Future<void> fetchUser() async {
-    var newUser = await userStorage.getUser();
+    var newUser = await userStorage.get();
     if (newUser.isSignedIn() != _user.isSignedIn()) {
       _user = newUser;
       notifyListeners();
     }
   }
 
+  Future<Token> fetchToken() async {
+    Token token = currentToken.isEmpty() ? await tokenStorage.get() : currentToken;
+    if (!token.isEmpty() && token.isRefreshNeeded()) {
+      token = await fetchFromRefresh();
+    }
+    return token;
+  }
+
+  Future<Token> fetchFromRefresh() async {
+    String refreshToken = _token.refreshToken!;
+
+    final response = await http.post(Uri.parse(authConfig.tokenUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        "grant_type": "refresh_token",
+        "refresh_token": refreshToken,
+      })
+    );
+
+    Map<String, dynamic> decodedResp = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      _token = Token.fromHttp(decodedResp);
+    } else if (response.statusCode > 500) {
+      notifyListeners();
+      throw Exception("Big fail ${decodedResp['error']} ${decodedResp['error_description']}");
+    } else {
+      logUserOut();
+    }
+    tokenStorage.insert(_token);
+    return _token;
+  }
+
   void logUserOut() {
-    userStorage.removeUser();
+    userStorage.remove();
+    tokenStorage.remove();
     _user = AnonymousUser();
+    _token = Token();
     notifyListeners();
   }
 
-  void logUserIn(User user) {
-    if (_user.email != user.email) {
-      _user = user;
+  Future<void> signUserUp(AnonymousUser user, Function onSuccess) async {
+    final signUpResp = await http.post(Uri.parse(authConfig.signUpUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        "user": {
+          ...user.toHttpMap(),
+        }
+      })
+    );
+    Map<String, dynamic> decodedResp = jsonDecode(signUpResp.body);
+    if (signUpResp.statusCode != 200) {
+        _user.errors = decodedResp['error_description'];
+        notifyListeners();
+    } else {
+      await signUserIn(user.email!, user.password!, onSuccess);
     }
+
+
+  }
+
+  Future<void> signUserIn(String email, String password, Function onLoginComplete) async {
+    Map<String, String?> body = {
+      "email": email,
+      "password": password,
+      "client_id": authConfig.clientId,
+      "client_secret": authConfig.clientSecret,
+      "grant_type": "password",
+    };
+
+    final response = await http.post(Uri.parse(authConfig.signInUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(body),
+    );
+    Map<String, dynamic> decodedResp = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      _user = User.fromMap({"email": email});
+      _token = Token.fromHttp(decodedResp);
+      onLoginComplete();
+    } else if (response.statusCode > 500) {
+      throw Exception("Big fail ${decodedResp['error']} ${decodedResp['error_description']}");
+    } else {
+      _token.errors = decodedResp['error_description'];
+    }
+    tokenStorage.insert(_token);
+    userStorage.insert(_user);
     notifyListeners();
-    userStorage.insertUser(user);
+  }
+
+  /// Performs a POST request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> post(String url,
+      {Map<String, String>? headers,
+        dynamic body,
+        http.Client? httpClient}) async {
+    return _request('POST', url,
+        headers: headers, body: body, httpClient: httpClient);
+  }
+
+  /// Performs a PUT request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> put(String url,
+      {Map<String, String>? headers,
+        dynamic body,
+        http.Client? httpClient}) async {
+    return _request('PUT', url,
+        headers: headers, body: body, httpClient: httpClient);
+  }
+
+  /// Performs a PATCH request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> patch(String url,
+      {Map<String, String>? headers,
+        dynamic body,
+        http.Client? httpClient}) async {
+    return _request('PATCH', url,
+        headers: headers, body: body, httpClient: httpClient);
+  }
+
+  /// Performs a GET request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> get(String url,
+      {Map<String, String>? headers, http.Client? httpClient}) async {
+    return _request('GET', url, headers: headers, httpClient: httpClient);
+  }
+
+  /// Performs a DELETE request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> delete(String url,
+      {Map<String, String>? headers, http.Client? httpClient}) async {
+    return _request('DELETE', url, headers: headers, httpClient: httpClient);
+  }
+
+  /// Performs a HEAD request to the specified [url], adding the authorization token.
+  ///
+  /// If no token already exists, or if it is expired, a new one is requested.
+  Future<http.Response> head(String url,
+      {Map<String, String>? headers,
+        dynamic body,
+        http.Client? httpClient}) async {
+    return _request('HEAD', url, headers: headers, httpClient: httpClient);
+  }
+
+  /// Common method for making http requests
+  /// Tries to use a previously fetched token, otherwise fetches a new token by means of a refresh flow or by issuing a new authorization flow
+  Future<http.Response> _request(String method, String url,
+      {Map<String, String>? headers,
+        dynamic body,
+        http.Client? httpClient}) async {
+    httpClient ??= http.Client();
+
+    headers ??= {};
+
+    sendRequest(accessToken) async {
+      http.Response resp;
+
+      headers!['Authorization'] = 'Bearer $accessToken';
+
+      if (method == 'POST') {
+        resp = await httpClient!
+            .post(Uri.parse(url), body: body, headers: headers);
+      } else if (method == 'PUT') {
+        resp =
+        await httpClient!.put(Uri.parse(url), body: body, headers: headers);
+      } else if (method == 'PATCH') {
+        resp = await httpClient!
+            .patch(Uri.parse(url), body: body, headers: headers);
+      } else if (method == 'GET') {
+        resp = await httpClient!.get(Uri.parse(url), headers: headers);
+      } else if (method == 'DELETE') {
+        resp = await httpClient!.delete(Uri.parse(url), headers: headers);
+      } else if (method == 'HEAD') {
+        resp = await httpClient!.head(Uri.parse(url), headers: headers);
+      } else {
+        throw Exception('Unknown method $method!');
+      }
+
+      return resp;
+    }
+
+    http.Response resp;
+
+    //Retrieve the current token, or fetches a new one if it is expired
+    var tknResp = await fetchToken();
+
+    try {
+      resp = await sendRequest(tknResp.accessToken);
+
+      if (resp.statusCode == 401) {
+        //The token could have been invalidated on the server side
+        //Try to fetch a new token...
+        if (tknResp.isRefreshNeeded()) {
+          tknResp = await fetchFromRefresh();
+        } else {
+          tknResp = await fetchToken();
+        }
+
+        if (tknResp.isValid()) {
+          resp = await sendRequest(tknResp.accessToken);
+        }
+      }
+    } catch (e) {
+      rethrow;
+    }
+    return resp;
   }
 }
